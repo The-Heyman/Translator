17e1152c307afebf414c0a24a79bbeb1309d5957
Alex Heyman <gameyalex@gmail.com>
2020-12-07 20:46:00 +0000

    refactoring code to a function for portability

--- src/translates.py
+++ src/translates.py
@@ -1,6 +1,24 @@
 import boto3
 
-client = boto3.client("translate", region_name="us-east-1")
-text = "Hvor gammel er du?"
-result = client.translate_text(Text=text, SourceLanguageCode="auto", TargetLanguageCode="en")
-print(result['TranslatedText'])
+def compute_translation(text, src="auto", target="en"):
+    
+    ''' Translate text from different supported languages to English by 
+        default or to an specified target. 
+        :params text: text to be translated.
+                src: language to be translated.
+                target: language to translate to.
+        :return: a translated string of the input text string
+    '''
+    
+    client = boto3.client("translate", region_name="us-east-1")
+    
+    result = client.translate_text(Text=text, SourceLanguageCode=src, 
+            TargetLanguageCode=target)
+    
+    return result['TranslatedText']
+    
+# test example
+
+compute_translation("Til at understøtte hjemmesidens", "auto","it")
+
+# the above translates from danish using auto detect to Italian as specified.
